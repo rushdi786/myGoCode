299ae0dfe85a73700991c86d3c184c9ce26bdacf
Student <student@mx23-hp>
2025-04-24 15:49:22 +0100

    third

---
+++ 03inputs/03pointers/04bufio/05printf/Task/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+	var Favoritebook string = ""
+	var Author string = ""
+	var Year string = ""
+
+	fmt.Printf(Favouritebook has value: &v and data type: %T\n", Favourite,Favourite)
+	fmt.Printf("Author has value: &v and data type: %T\n", Author,Author)
+	fmt.Printf(Year has value: &v and data type: %T\n", Year,Year)
+	
+}
---
+++ 03inputs/03pointers/04bufio/05printf/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "joanne"
+	var age int = 30
+
+	fmt.Printf("name has value: %v and data type: %T\n", age, age)
+	fmt.Print("age has value: %v and data type: %T\n", name, name)
+}
